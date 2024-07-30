72bfc7440837ebc499db96ef241d5f230ce46ade
Amit yadav <109620531+amityadav23112000@users.noreply.github.com>
2024-07-30 13:14:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import InputBox from "./Components/Input"
+import Shopping from "./Components/Shopping"
+
+
+function App() {
+  // react fragment
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <Shopping></Shopping>
+    </div>
+
+  )
+}
+
+export default App
